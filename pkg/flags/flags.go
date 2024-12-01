5a1eb4b90f9999101aa5babbd5db421e83b5f42d
meDracula <me.dracula@protonmail.com>
2024-12-01 17:17:25 +0100

    Fix flags pointer issue

--- pkg/flags/flags.go
+++ pkg/flags/flags.go
@@ -3,15 +3,15 @@ package flags
 import "flag"
 
 type FlagOutputOpts struct {
-	Day int
-	File string
+	Day   int
+	File  string
 	Debug bool
 }
 
 func Parse() (opts *FlagOutputOpts) {
-	flag.IntVar(opts.Day, "day", 0, "Select day to run")
-	flag.StringVar(opts.File, "file", "", "Path to solve puzzle input")
-	flag.BoolVar(opts.Debug, "debug", false, "log debug")
+	flag.IntVar(&opts.Day, "day", 0, "Select day to run")
+	flag.StringVar(&opts.File, "file", "", "Path to solve puzzle input")
+	flag.BoolVar(&opts.Debug, "debug", false, "log debug")
 
 	flag.Parse()
 	return opts
