2a8f13383f0c2f2dd1b14f16afd5216cabb9f029
meDracula <me.dracula@protonmail.com>
2024-12-08 15:42:15 +0100

    Advent of Code Day 5 Part 2 with refactoring on Part 1

--- days/day5/day5.go
+++ days/day5/day5.go
@@ -1,12 +1,14 @@
 package day5
 
 import (
-	"aoc2024/pkg/log"
 	"bufio"
 	"fmt"
 	"os"
+	"reflect"
 	"strconv"
 	"strings"
+
+	"aoc2024/pkg/log"
 )
 
 type Rules map[string]bool
@@ -54,21 +56,14 @@ func extractUpdateManual(filename string) (Rules, []Update) {
 	return rules, updates
 }
 
-func (r Rules) validPage(page []string) bool {
+func (r Rules) validUpdate(update Update) bool {
+	page := update.page
 	for i := 0; i < (len(page) - 1); i++ {
 		log.Debug("Check valid page", log.Any("page", page[i:]), log.Int("index", i))
 
-		// Code logic edge case: if len of page is 2 then check rules loop condition will be false i+1 < len(page[i:])
-		if len(page[i:]) == Two {
-			key := page[i] + "|" + page[i+1]
-			if _, ok := r[key]; !ok {
-				log.Debug("Rule doesn't exist", log.String("key", key))
-				return false
-			}
-		}
-
 		// Check rules loop
-		for j := i + 1; j < len(page[i:]); j++ {
+		// Code logic edge case: if len of page is 2 then check rules loop condition will be false i+1 < len(page[i:])
+		for j := i + 1; j < len(page); j++ {
 			key := page[i] + "|" + page[j]
 			if _, ok := r[key]; !ok {
 				log.Debug("Rule doesn't exist", log.String("key", key))
@@ -85,12 +80,60 @@ func (r Rules) updateOrdering(updates []Update) []Update {
 	for _, update := range updates {
 		log.Debug("Check valid Update", log.Any("page", update.page))
 
-		if r.validPage(update.page) {
+		if r.validUpdate(update) {
 			log.Debug("update is valid", log.Any("page", update.page))
 			validUpates = append(validUpates, update)
 		}
 	}
 	return validUpates
+}
+
+func (r Rules) patchPage(page []string) bool {
+	for i := 0; i < (len(page) - 1); i++ {
+		log.Debug("Check if patch required", log.Any("page", page[i:]), log.Int("index", i))
+		// Check rules loop
+		// Code logic edge case: if len of page is 2 then check rules loop condition will be false i+1 < len(page[i:])
+		for j := i + 1; j < len(page); j++ {
+			key := page[i] + "|" + page[j]
+
+			log.Debug("Check key patch required",
+				log.Any("page[i]", page[i]),
+				log.Any("page[j]", page[j]),
+				log.String("key", key),
+				log.Int("index", i),
+			)
+			if _, ok := r[key]; !ok {
+				// Swap key value
+				page[i], page[j] = page[j], page[i]
+				log.Debug("Swap keyed", log.String("key", key), log.Any("Page", page))
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func (r Rules) fixUpdate(update *Update) {
+	var (
+		page    = update.page
+		patched = false
+	)
+	for !patched {
+		log.Debug("Run update patch page", log.Any("page", page))
+		if r.patchPage(page) {
+			update.page = page
+			log.Debug("Patched complete", log.Any("Page", page))
+			patched = true
+		}
+	}
+	return
+}
+
+func (r Rules) fixIncorrectlyUpdates(updates []Update) []Update {
+	for _, update := range updates {
+		r.fixUpdate(&update)
+	}
+	return updates
 }
 
 func sumUpdates(updates []Update) int {
@@ -113,6 +156,22 @@ func sumUpdates(updates []Update) int {
 	return sum
 }
 
+func FilterUpdate(updates []Update, remove []Update) []Update {
+	filtered := []Update{}
+	for _, update := range updates {
+		add := true
+		for _, filter := range remove {
+			if reflect.DeepEqual(update, filter) {
+				add = false
+			}
+		}
+		if add {
+			filtered = append(filtered, update)
+		}
+	}
+	return filtered
+}
+
 func AdventSolveDay5(filename string) {
 	log.Info("Start Part 1", log.String("filename", filename))
 
@@ -122,4 +181,14 @@ func AdventSolveDay5(filename string) {
 
 	fmt.Println("correctly-ordered updates sum:", sum)
 	log.Info("Done Part 1", log.String("filename", filename))
+
+	incorrectlyUpdates := FilterUpdate(updates, updateOrdering)
+
+	log.Info("Start Part 2", log.String("filename", filename))
+
+	fixedUpdateOrdering := rules.fixIncorrectlyUpdates(incorrectlyUpdates)
+	sum = sumUpdates(fixedUpdateOrdering)
+
+	fmt.Println("sorted correctly-ordered updates sum:", sum)
+	log.Info("Done Part 2", log.String("filename", filename))
 }
